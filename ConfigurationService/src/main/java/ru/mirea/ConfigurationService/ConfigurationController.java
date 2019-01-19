fb04012392188993ab94ea08a73957ac150e61cf
SG_Muwa <motherlode.muwa@gmail.com>
2019-01-19 22:23:28 +0300

    Контроллер конфигураций возвращает файлы

--- ConfigurationService/src/main/java/ru/mirea/ConfigurationService/ConfigurationController.java
+++ ConfigurationService/src/main/java/ru/mirea/ConfigurationService/ConfigurationController.java
@@ -1,18 +1,18 @@
 package ru.mirea.ConfigurationService;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
@@ -24,13 +24,17 @@ public class ConfigurationController {
     public ResponseEntity<String> PropertiesCollector(@PathVariable String filename) {
         // Проверка, что файл начинается с буквы. Допускаются английские названия, точки.
         // Заканчивается всегда на .properties.
-        if(!filename.matches("[a-zA-Z]([a-zA-Z.0-9])?+\\.properties"))
+        if(!filename.matches("[a-zA-Z]([a-zA-Z.0-9]+)?\\.properties"))
             return new ResponseEntity<>(BAD_REQUEST); // Файл не прошёл проверку. Защита от иньекций.
         try(InputStream stream =
-                    ConfigurationApplication.class.getResourceAsStream(filename + ".txt")) {
-            return new ResponseEntity<>(new String(stream.readAllBytes()), OK);
-        } catch (IOException e) {
-            return new ResponseEntity<>(NOT_FOUND); // Такого файла не существует.
+                    new FileInputStream(new ClassPathXmlApplicationContext().getResource(filename + ".txt").getFile())) {
+            String content = new String(stream.readAllBytes());
+            return ResponseEntity.ok()
+                    .contentLength(content.length())
+                    .contentType(MediaType.TEXT_EVENT_STREAM)
+                    .body(content);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), NOT_FOUND); // Такого файла не существует.
         }
     }
 }
