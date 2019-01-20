9ba05b4c83d26199a511f68a02942b674fe39f60
SG_Muwa <motherlode.muwa@gmail.com>
2019-01-20 21:12:53 +0300

    Удаление фильтра админ карт

--- CartService/src/main/java/ru/mirea/CartService/ServiceForCart.java
+++ CartService/src/main/java/ru/mirea/CartService/ServiceForCart.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class ServiceForCart {
     private CartDbConnection cartConnect;
     private String tokenStr;
-    final String secret_key = "sdkfda";
+    private final String secret_key = "sdkfda";
     @Autowired
     public ServiceForCart(CartDbConnection cartConnect/*,RESTtEMPLATE */){
         this.cartConnect = cartConnect;
