ab4abee2df45a31e921dc6819962518ca038e159
SG_Muwa <motherlode.muwa@gmail.com>
2019-01-24 20:20:02 +0300

    Всё ещё не получается запустить тест

--- TestClientConfiguration/src/test/java/ru/mirea/TestClientConfiguration/TestControllerTest.java
+++ TestClientConfiguration/src/test/java/ru/mirea/TestClientConfiguration/TestControllerTest.java
@@ -1,23 +1,28 @@
 package ru.mirea.TestClientConfiguration;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TestControllerTest {
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestControllerTest {
 
-    @LocalServerPort
-    int randomServerPort;
+    public TestControllerTest() {
 
-    @org.junit.jupiter.api.Test
-    void getProperties() {
+    }
+
+    //@LocalServerPort
+    //int randomServerPort;
+
+    @Test
+    public void getProperties() {
         /*
         assertEquals(7000, randomServerPort);
         RestTemplate abs = new RestTemplate();
