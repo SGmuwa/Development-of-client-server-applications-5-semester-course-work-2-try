0cc234fbd2279101536ba9224379a1b2617074c4
SGmuwa <motherlode.muwa@gmail.com>
2019-01-25 23:07:27 +0300

    В таком состоянии тест проходится

--- TestClientConfiguration/src/test/java/ru/mirea/TestClientConfiguration/TestControllerTest.java
+++ TestClientConfiguration/src/test/java/ru/mirea/TestClientConfiguration/TestControllerTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = Application.class, loader = AnnotationConfigContextLoader.class)
 public class TestControllerTest {
 
     public TestControllerTest() {
