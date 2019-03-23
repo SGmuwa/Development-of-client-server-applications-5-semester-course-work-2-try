887cfa73a5b09ae78bb1f42bf2a296725807ae1a
SG_Muwa <motherlode.muwa@gmail.com>
2019-03-23 09:04:49 +0300

    Исправлена отдельная компиляция Токенов

--- Tokens/src/test/java/ru/mirea/Tokens/PayloadTokenFactoryTest.java
+++ Tokens/src/test/java/ru/mirea/Tokens/PayloadTokenFactoryTest.java
@@ -11,5 +11,7 @@ public class PayloadTokenFactoryTest {
     public void SaveAndLoadPayload() {
         PayloadToken payloadToken = new PayloadToken(2, "pup", Collections.singletonList(Role.ADMIN));
         assertEquals("{\"id\":2,\"login\":\"pup\",\"role\":[\"ADMIN\"]}", payloadToken.generateJSON());
+        System.out.println(new PayloadToken(3, "SidorenkoMikhail", Collections.singletonList(Role.USER)));
+        System.out.println(TokenFactory.generateToken(new PayloadToken(3, "SidorenkoMikhail", Collections.singletonList(Role.USER))));
     }
 }
